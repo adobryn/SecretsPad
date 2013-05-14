8fc0f7fe8f5b3edd6e612212173f0146e56b7fa8
Alexandra <a.dobryn@gmail.com>
2013-05-14 19:18:18 +0400

    Comparing coords

--- src/com/dobrynland/secretspad/MainTouchView.java
+++ src/com/dobrynland/secretspad/MainTouchView.java
@@ -10,18 +10,22 @@ import android.widget.ImageView;
 import android.view.MotionEvent;
 import android.graphics.RectF;
 import android.widget.Toast;
+import android.graphics.PathMeasure;
+import android.graphics.Matrix;
 
 
 public class MainTouchView extends ImageView
 {
     private Paint paint = new Paint();
     private Path path = new Path();
+    private PathMeasure pathMeasure = new PathMeasure(path, false);
+    private Matrix matrix = new Matrix();
 
     public MainTouchView(Context context)
     {
         super(context);
 
-        //paint.setAntiAlias(true);
+        paint.setAntiAlias(true);
         paint.setStrokeWidth(6f);
         paint.setColor(Color.BLUE);
         paint.setStyle(Paint.Style.STROKE);
@@ -44,13 +48,24 @@ public class MainTouchView extends ImageView
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 path.moveTo(eventX, eventY);
-                path.addCircle(eventX, eventY,10, Path.Direction.CW);
+                path.addCircle(eventX, eventY, 20, Path.Direction.CW);
                 return true;
             case MotionEvent.ACTION_MOVE:
                 //path.lineTo(eventX, eventY);
                 break;
             case MotionEvent.ACTION_UP:
-                //path.addCircle(eventX, eventY,10, Path.Direction.CW);
+                float aCoordinates[] = {0f, 0f};
+                float bCoordinates[] = {eventX, eventY};
+
+                //pathMeasure.nextContour();
+                pathMeasure.setPath(path, false);
+
+                //get point from the middle
+                pathMeasure.getPosTan(pathMeasure.getLength() * 0.5f, aCoordinates, null);
+                double dist = distance(aCoordinates, bCoordinates);
+                if(dist < 25)
+                    ToastMsg("OK");
+                //path.reset();
                 break;
             default:
                 return false;
@@ -64,7 +79,32 @@ public class MainTouchView extends ImageView
     public void ToastMsg(String str)
     {
         Toast msg = Toast.makeText(MainTouchView.this.getContext(), str, Toast.LENGTH_SHORT);
-        msg.setGravity(Gravity.CENTER, msg.getXOffset() / 2, msg.getYOffset() / 2);
+        msg.setGravity(Gravity.BOTTOM, msg.getXOffset() / 2, msg.getYOffset() / 2);
         msg.show();
+    }
+
+    static float[] computeCentroid(float[] points)
+    {
+        float centerX = 0;
+        float centerY = 0;
+        int count = points.length;
+        for (int i = 0; i < count; i++) {
+            centerX += points[i];
+            i++;
+            centerY += points[i];
+        }
+        float[] center = new float[2];
+        center[0] = 2 * centerX / count;
+        center[1] = 2 * centerY / count;
+
+        return center;
+    }
+
+    public double distance(float[] p, float[] q)
+    {
+        double dx   = p[0] - q[0];         //horizontal difference
+        double dy   = p[1] - q[1];         //vertical difference
+        double dist = Math.sqrt( dx*dx + dy*dy ); //distance using Pythagoras theorem
+        return dist;
     }
 }
