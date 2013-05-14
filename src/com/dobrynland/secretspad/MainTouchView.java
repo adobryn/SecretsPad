eeb225cde7f4bf71c83d0f87c55f727b9b8424e3
Alexandra <a.dobryn@gmail.com>
2013-05-14 15:14:43 +0400

    Lines paint

--- src/com/dobrynland/secretspad/MainTouchView.java
+++ src/com/dobrynland/secretspad/MainTouchView.java
@@ -4,199 +4,64 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.util.TypedValue;
+import android.graphics.Path;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
 
 
-public class MainTouchView extends View implements View.OnLongClickListener, View.OnClickListener
+public class MainTouchView extends View
 {
+    private Paint paint = new Paint();
+    private Path path = new Path();
 
     public MainTouchView(Context context)
     {
         super(context);
-        this.setOnLongClickListener(this);
-        this.setOnClickListener(this);
-        paint.setColor(Color.WHITE);
+        paint.setAntiAlias(true);
+        paint.setStrokeWidth(6f);
+        paint.setColor(Color.BLACK);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
     }
 
     @Override
-    public void onDraw(Canvas canvas) {
-        if(downX > 0)
-        {
-            paint.setStyle(Paint.Style.FILL);
-            canvas.drawCircle(downX, downY, getScreenSize(touchCircleRadius), paint);
-            paint.setStyle(Paint.Style.STROKE);
-            canvas.drawCircle(downX, downY, getScreenSize(touchCircleRadius + pressureRingOffset + (pressureRingOffset * pressure)), paint);
-        }
-
+    public void onDraw(Canvas canvas)
+    {
+        canvas.drawPath(path, paint);
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if(callBaseClass)
-        {
-            super.onTouchEvent(event);
-        }
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        float eventX = event.getX();
+        float eventY = event.getY();
 
-        if(!handleOnTouchEvent)
-        {
-            return false;
-        }
-
-        int action = event.getAction();
-        pressure = event.getPressure() * pressureAmplification;
-
-        boolean result = false;
-        switch (action)
-        {
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                downX = event.getX();
-                downY = event.getY();
-                if (returnValueOnActionDown)
-                {
-                    result = returnValueOnActionDown;
-                }
-                break;
+                path.moveTo(eventX, eventY);
+                return true;
             case MotionEvent.ACTION_MOVE:
-                downX = event.getX();
-                downY = event.getY();
-                if (returnValueOnActionMove)
-                {
-                    result = returnValueOnActionMove;
-                }
+                path.lineTo(eventX, eventY);
                 break;
             case MotionEvent.ACTION_UP:
-                downX = -1;
-                downY = -1;
-                if (returnValueOnActionUp)
-                {
-                    result = returnValueOnActionUp;
-                }
+                // nothing to do
                 break;
-            case MotionEvent.ACTION_OUTSIDE:
-                break;
+            default:
+                return false;
         }
+
+        // Schedules a repaint.
         invalidate();
-        return result;
+
+        return true;
     }
 
-    @Override
-    public void onClick(View v) {
-        Toast msg = Toast.makeText(MainTouchView.this.getContext(), "onClick", Toast.LENGTH_SHORT);
+    public void ToastMsg(String str)
+    {
+        Toast msg = Toast.makeText(MainTouchView.this.getContext(), str, Toast.LENGTH_SHORT);
         msg.setGravity(Gravity.CENTER, msg.getXOffset() / 2, msg.getYOffset() / 2);
         msg.show();
     }
-
-    @Override
-    public boolean onLongClick(View v) {
-        Toast msg = Toast.makeText(MainTouchView.this.getContext(), "onLongClick", Toast.LENGTH_SHORT);
-        msg.setGravity(Gravity.CENTER, msg.getXOffset() / 2, msg.getYOffset() / 2);
-        msg.show();
-        return returnValueOnLongClick;
-    }
-
-    public float getScreenSize(float lengthInMm)
-    {
-        //Size_in_mm = Size_in_inches * 25.4;
-        //Size_in_inches = Size_in_mm / 25.4;
-        //Size_in_dp = Size_in_inches * 160;
-        //Size_in_dp = (Size_in_mm / 25.4) * 160;
-        //Size_in_inches = Size_in_dp / 160;
-        return TypedValue.applyDimension(
-                TypedValue.COMPLEX_UNIT_DIP,
-                (float)(lengthInMm * 160 / 25.4),
-                getResources().getDisplayMetrics());
-    }
-
-    public void setCallBaseClass(boolean process)
-    {
-        callBaseClass = process;
-    }
-
-    public boolean getCallBaseClass()
-    {
-        return callBaseClass;
-    }
-
-    public void setHandleTouchEvent(boolean process)
-    {
-        handleOnTouchEvent = process;
-    }
-
-    public boolean getHandleTouchEvent()
-    {
-        return handleOnTouchEvent;
-    }
-
-    public void setReturnValueOnActionDown(boolean value)
-    {
-        returnValueOnActionDown = value;
-    }
-
-    public boolean getReturnValueOnActionDown()
-    {
-        return returnValueOnActionDown;
-    }
-
-    public void setReturnValueOnActionMove(boolean value)
-    {
-        returnValueOnActionMove = value;
-    }
-
-    public boolean getReturnValueOnActionMove()
-    {
-        return returnValueOnActionMove;
-    }
-
-    public void setReturnValueOnActionUp(boolean value)
-    {
-        returnValueOnActionUp = value;
-    }
-
-    public boolean getReturnValueOnActionUp()
-    {
-        return returnValueOnActionUp;
-    }
-
-    public void setPressureAmplification(float value)
-    {
-        pressureAmplification = value;
-    }
-
-    public float getPressureAmplification()
-    {
-        return pressureAmplification;
-    }
-
-    public void setReturnValueOnLongClick(boolean value)
-    {
-        returnValueOnLongClick = value;
-    }
-
-    public boolean getReturnValueOnLongClick()
-    {
-        return returnValueOnLongClick;
-    }
-
-    private float touchCircleRadius = (float) DefaultValues.TouchCircleRadius;
-    private float pressureRingOffset = (float) DefaultValues.PressureRingOffset;
-    private float pressureAmplification = (float) DefaultValues.PressureAmplificaton;
-
-    private Paint paint = new Paint();
-
-    private float downX = -1;
-    private float downY = -1;
-    private float pressure = 1;
-
-    private boolean callBaseClass = true;
-    private boolean handleOnTouchEvent = true;
-    private boolean returnValueOnActionDown = true;
-    private boolean returnValueOnActionMove = true;
-    private boolean returnValueOnActionUp = true;
-    private boolean returnValueOnLongClick = false;
-
-
 }
